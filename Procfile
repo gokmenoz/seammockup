76c52c571a7b6e9554ea6255b480420e60f0d6eb
unknown <gokmenoz89@gmail.com>
2019-04-24 21:35:36 -0400

    first

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bokeh serve --port=$PORT --num-procs=0 --allow-websocket-origin=seamsocial.herokuapp.com --address=0.0.0.0 --use-xheaders Capstone.py
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request
+   
+
+app = Flask(__name__)
+
+@app.route('/')
+def index_lulu():
+    return render_template('tabs.html')
+    
+@app.route('/static/<path:path>')
+def static_file(path):
+    return app.send_static_file(os.path.join('static', path))
+
+if __name__ == "__main__":
+    app.debug=False
+    app.run(host='0.0.0.0')
