f431d4332398d663a51700f696a3222cfe94f59b
unknown <gokmenoz89@gmail.com>
2019-04-25 19:12:57 -0400

    'h'

--- application.py
+++ application.py
@@ -3,11 +3,11 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route('/',method=['GET','POST'])
+@app.route('/')
 def index_lulu():
     return render_template('tabs.html')
 
-@app.route('/district101.html')
+@app.route('/district101.html',method=['POST'])
 def dist_lulu():
     return render_template('district101.html')
 
