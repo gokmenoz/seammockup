fe9992cf8edf2d5359182aa08ba4c2d77dcb7968
unknown <gokmenoz89@gmail.com>
2019-04-25 20:03:52 -0400

    'h'

--- application.py
+++ application.py
@@ -3,11 +3,12 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route('/',method=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def index_lulu():
     return render_template('tabs.html')
+    
 
-@app.route('/district101.html',method=['GET','POST'])
+@app.route('/district101.html',methods=['GET','POST'])
 def dist_lulu():
     return render_template('district101.html')
 
@@ -16,5 +17,4 @@ def static_file(path):
     return app.send_static_file(os.path.join('static', path))
 
 if __name__ == "__main__":
-    app.debug=False
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
